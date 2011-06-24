1b735bc5d5294b93708cd51388bcfcfb7b6dc376
Daniel Grech <danielgrech91@gmail.com>
2011-06-24 21:15:11 +0800

    Added files needed for PopupActions

--- gen/com/DGSD/TweeterTweeter/R.java
+++ gen/com/DGSD/TweeterTweeter/R.java
@@ -8,42 +8,93 @@
 package com.DGSD.TweeterTweeter;
 
 public final class R {
+    public static final class anim {
+        public static final int disappear=0x7f040000;
+        public static final int grow_from_bottom=0x7f040001;
+        public static final int grow_from_bottomleft_to_topright=0x7f040002;
+        public static final int grow_from_bottomright_to_topleft=0x7f040003;
+        public static final int grow_from_top=0x7f040004;
+        public static final int grow_from_topleft_to_bottomright=0x7f040005;
+        public static final int grow_from_topright_to_bottomleft=0x7f040006;
+        public static final int pump_bottom=0x7f040007;
+        public static final int pump_top=0x7f040008;
+        public static final int rail=0x7f040009;
+        public static final int rotate=0x7f04000a;
+        public static final int shrink_from_bottom=0x7f04000b;
+        public static final int shrink_from_bottomleft_to_topright=0x7f04000c;
+        public static final int shrink_from_bottomright_to_topleft=0x7f04000d;
+        public static final int shrink_from_top=0x7f04000e;
+        public static final int shrink_from_topleft_to_bottomright=0x7f04000f;
+        public static final int shrink_from_topright_to_bottomleft=0x7f040010;
+    }
     public static final class array {
-        public static final int view_types=0x7f050000;
+        public static final int view_types=0x7f060000;
     }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int favourite_gold=0x7f020000;
-        public static final int ic_contact_picture=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int twitter_icon=0x7f020003;
-        public static final int word_icon=0x7f020004;
+        public static final int action_item_btn=0x7f020000;
+        public static final int action_item_selected=0x7f020001;
+        public static final int arrow_down=0x7f020002;
+        public static final int arrow_up=0x7f020003;
+        public static final int favourite_gold=0x7f020004;
+        public static final int ic_contact_picture=0x7f020005;
+        public static final int icon=0x7f020006;
+        public static final int popup=0x7f020007;
+        public static final int twitter_icon=0x7f020008;
+        public static final int word_icon=0x7f020009;
     }
     public static final class id {
-        public static final int container=0x7f060004;
-        public static final int favourite_star=0x7f06000a;
-        public static final int list=0x7f060000;
-        public static final int listItem=0x7f060005;
-        public static final int loginButton=0x7f060003;
-        public static final int loginLogo=0x7f060002;
-        public static final int loginfragmentContainer=0x7f060001;
-        public static final int timeline_date=0x7f060008;
-        public static final int timeline_favourite_star=0x7f06000b;
-        public static final int timeline_profile_image=0x7f060006;
-        public static final int timeline_source=0x7f060007;
-        public static final int timeline_tweet=0x7f060009;
+        public static final int arrow_down=0x7f08000a;
+        public static final int arrow_up=0x7f080009;
+        public static final int container=0x7f080006;
+        public static final int favourite_star=0x7f080010;
+        public static final int icon=0x7f080000;
+        public static final int list=0x7f080002;
+        public static final int listItem=0x7f08000b;
+        public static final int loginButton=0x7f080005;
+        public static final int loginLogo=0x7f080004;
+        public static final int loginfragmentContainer=0x7f080003;
+        public static final int scroller=0x7f080007;
+        public static final int timeline_date=0x7f08000e;
+        public static final int timeline_favourite_star=0x7f080011;
+        public static final int timeline_profile_image=0x7f08000c;
+        public static final int timeline_source=0x7f08000d;
+        public static final int timeline_tweet=0x7f08000f;
+        public static final int title=0x7f080001;
+        public static final int tracks=0x7f080008;
     }
     public static final class layout {
-        public static final int list_fragment_layout=0x7f030000;
-        public static final int login_fragment_layout=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int timeline_list_item=0x7f030003;
+        public static final int action_item=0x7f030000;
+        public static final int list_fragment_layout=0x7f030001;
+        public static final int login_fragment_layout=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int popup=0x7f030004;
+        public static final int timeline_list_item=0x7f030005;
     }
     public static final class string {
-        public static final int account_spinner=0x7f040003;
-        public static final int app_name=0x7f040000;
-        public static final int consumer_key=0x7f040001;
-        public static final int consumer_secret=0x7f040002;
+        public static final int account_spinner=0x7f050003;
+        public static final int app_name=0x7f050000;
+        public static final int consumer_key=0x7f050001;
+        public static final int consumer_secret=0x7f050002;
+    }
+    public static final class style {
+        public static final int Animations=0x7f070000;
+        public static final int Animations_GrowFromBottom=0x7f070001;
+        public static final int Animations_GrowFromTop=0x7f070002;
+        /**  PopDownMenu 
+         */
+        public static final int Animations_PopDownMenu=0x7f070003;
+        public static final int Animations_PopDownMenu_Center=0x7f070004;
+        public static final int Animations_PopDownMenu_Left=0x7f070005;
+        public static final int Animations_PopDownMenu_Reflect=0x7f070007;
+        public static final int Animations_PopDownMenu_Right=0x7f070006;
+        /**  PopUpMenu 
+         */
+        public static final int Animations_PopUpMenu=0x7f070008;
+        public static final int Animations_PopUpMenu_Center=0x7f070009;
+        public static final int Animations_PopUpMenu_Left=0x7f07000a;
+        public static final int Animations_PopUpMenu_Reflect=0x7f07000c;
+        public static final int Animations_PopUpMenu_Right=0x7f07000b;
     }
 }
